2d4419eefd73943d36ef568e553c0f50ba665983
Ivan <ivan248959@gmail.com>
2018-01-27 18:33:21 +0100

    testggg

---
+++ src/main/java/com/isa/project/Hello.java
@@ -0,0 +1,19 @@
+package com.isa.project;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class Hello {
+
+	 @RequestMapping(method=RequestMethod.GET)
+	 public @ResponseBody String sayHello() {
+	        return "Hellosfs!";
+	    }
+}
