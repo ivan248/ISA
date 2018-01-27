a917a5e6c19e66dfcfaccace0929525ae34e1be6
Ivan <ivan248959@gmail.com>
2018-01-27 18:36:06 +0100

    yryryr

--- src/main/java/com/isa/project/Hello.java
+++ src/main/java/com/isa/project/Hello.java
@@ -14,6 +14,6 @@ public class Hello {
 
 	 @RequestMapping(method=RequestMethod.GET)
 	 public @ResponseBody String sayHello() {
-	        return "Hellosfkks!";
+	        return "Hellosfjykks!";
 	    }
 }
