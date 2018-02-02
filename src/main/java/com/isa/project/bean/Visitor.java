fdc5049cd29262875e173ebf5cbf7a055532b395
Ivan <ivan248959@gmail.com>
2018-02-02 12:20:54 +0100

    Connection with DB.

--- src/main/java/com/isa/project/bean/Visitor.java
+++ src/main/java/com/isa/project/bean/Visitor.java
@@ -1,5 +1,38 @@
 package com.isa.project.bean;
 
-public class Visitor {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "Visitor")
+public class Visitor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	@Column
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
--- src/main/java/com/isa/project/repository/VisitorRepository.java
+++ src/main/java/com/isa/project/repository/VisitorRepository.java
@@ -1,5 +1,11 @@
 package com.isa.project.repository;
 
-public interface VisitorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.isa.project.bean.Visitor;
+
+@Repository
+public interface VisitorRepository extends JpaRepository<Visitor, Long> {
 
 }
