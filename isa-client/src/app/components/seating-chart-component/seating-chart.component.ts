28f9a720d8d75cf63020e9eedbcf26ae74bbdc71
Ivan <ivan248959@gmail.com>
2018-04-11 12:52:34 +0200

    Working on seat reservation algorithm.

--- isa-client/src/app/components/seating-chart-component/seating-chart.component.ts
+++ isa-client/src/app/components/seating-chart-component/seating-chart.component.ts
@@ -10,12 +10,57 @@ import { Router } from '@angular/router';
 export class SeatingChartComponent implements OnInit {
 
 
+    private buttonClickedId : number = -1;
+    private buttonsArray : number[] = [];
+
     constructor(private router : Router) {
 
+      for(var i : number = 0; i<13; i++)
+      {
+        this.buttonsArray[i] = 2;
+      }
+
+      this.buttonsArray[3] = -1;
+      this.buttonsArray[6] = -1;
     }
 
     ngOnInit() {
 
+    }
+
+    buttonClicked(id : number) {
+      this.buttonClickedId = id;
+      console.log(id + " buttonClicked");
+
+      console.log("pre ifa " + this.buttonsArray[id]);
+      if(this.buttonsArray[id] != -1)
+        this.buttonsArray[id]++;
+      console.log("posle ifa " + this.buttonsArray[id]);
+    }
+
+    checkClass(idButton : number) {
+
+      if(this.buttonsArray[idButton]%2 === 0)
+      {
+        return "btn btn-success";
+      }
+
+      if(this.buttonsArray[idButton]%2 === -1)
+      {
+        return "btn btn-secondary disabled";
+      }
+      
+      return "btn btn-primary";
+    }
+
+    nextStep() {
+      for(var i:number=0; i<this.buttonsArray.length; i++)
+      {
+        if(this.buttonsArray[i]%2 === 1)
+        {
+          console.log(i);
+        }
+      }
     }
 
 }
