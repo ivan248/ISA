faeb5081ebe2dd6a5a9dd36c89e5e9e97f354d3a
Filip Petrovic <fpetrovic@ymail.com>
2018-04-16 15:26:10 +0200

    Commit

--- src/main/java/com/isa/project/service/ItemService.java
+++ src/main/java/com/isa/project/service/ItemService.java
@@ -9,9 +9,10 @@ public interface ItemService {
 
 	Boolean editItem(Item item);
 	Boolean approveItem(int id);
-	Boolean bid(int id,float bid);
+	Boolean bid(int id,float bid,int version) throws Exception;
 	Boolean acceptBid(int id); //korisnik koji je okacio oglas prihvata ponudu(prodaje)
 	Boolean checkApproved(int id); //provera da li je rekvizit odobren pre nego sto se izmeni
+	
 	
 	//TODO: vraca sve ponude prijavljenog korisnika
 	
